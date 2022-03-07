d3ce54bfe4cfeea1bd8e201d9d6ba75cd7a65bbf
znyrpt1 <znyrpt1@gmail.com>
2022-03-07 06:15:18 +0000

    4th

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with redux
+          Learn React with fds redux
         </a>
       </header>
     </div>
